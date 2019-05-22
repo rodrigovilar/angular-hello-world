ce5c50669c820d00aa9f583bf41ba64e1226d388
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2019-05-21 23:27:45 -0300

    Dividindo responsabilidades em 2 Componentes

---
+++ src/app/usuario/usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input }
+  from '@angular/core';
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent implements OnInit {
+  @Input() name: string;
+  constructor() { }
+  ngOnInit() { }
+}
