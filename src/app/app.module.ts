8001c4fc948f923bc1801e3e97060186796b16df
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2019-05-21 23:00:33 -0300

    Criando componentes e usando dados nos templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MensagemComponent } from './mensagem/mensagem.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MensagemComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+
+  name: string;
+
+  constructor() { 
+    this.name = ' Bill';
+  }
+
+  ngOnInit() {
+  }
+
+}
